aea5b013c78252e1b9bcb44941dd924043ada131
Darwin Betancourt <drw@drwmobile.drwsoluciones.net>
2013-04-30 21:27:29 -0500

    subiendo archivos para manejo de responsive menu en el tema cbftz

--- sites/all/themes/cbftz/js/cbftz.js
+++ sites/all/themes/cbftz/js/cbftz.js
@@ -1,39 +1,50 @@
 (function ($) {
-	// body...
-	/*Drupal.behaviors.mainMenuToggle = {
-		attach: function($context, $settings){
-			('.region-menu .navigation').before('<a href="#" class="menu-toggle" title="Toggle Menu"')
-		}
-	};*/
-	Drupal.behaviors.mainMenuToggle = {
-    attach: function(context, settings) {            
-      $('.region-menu .navigation').before('<a href="#" class="menu-toggle" title="Toggle Menu"><span class="line first-line first"></span><span class="line"></span><span class="line"></span><span class="line last-line last"></span><span class="toggle-help">Menu</span></a>');
-      $('.navigation .primary-menu h2, .navigation .second-menu h2').removeClass('element-invisible');
-      
-      $('.region-menu .menu-toggle').click(function(){
-        $('.navigation').slideToggle();
+	Drupal.behaviors.yourfunction = {
+    attach: function(context) {
+      $('body', context).once('accordionmenu', function () {
+        $('body').bind('responsivelayout', function (e, d) {        
+         
+        //Define your drupal menu id (only works with core menu block)
+          var menuid = "#block-system-main-menu";        
+         
+        //This condition will act under the 'mobile' size, and will not be executed on ie6 and ie7
+          if (d.to == 'mobile' && !$('html').hasClass('ie6') && !$('html').hasClass('ie7')) {       
+           
+             //Remove the ´hover´ event in the dropdown menu scrip below              
+             $(menuid+' li').unbind();
+              
+             //Add a span tag that will aft as the expand button, you can change the output of that button here
+             $(menuid+" ul.menu li.expanded").prepend( $("<span class='over' href='#'>&darr;</span>") );     
+                      
+             //Create an open/close action on the accordion after clicking on the expand element         
+             $(menuid+' ul.menu span.over').click(function (event) {     
+                 event.preventDefault();
+                 if ($(this).siblings('ul').is( ":visible" )){
+                 $(this).siblings('ul').slideUp('fast');  
+                 } else {           
+                 $(this).siblings('ul').slideDown('fast');  
+             }                       
+             });             
+          }               
+          //this condition will work for all sizes exept mobile, but will act on ie6 and ie7 browsers     
+          if (d.to != 'mobile' | $('html').hasClass('ie7') ) {            
+             //remove the expand elements from the accordion menu
+             $(menuid+" span.over").remove();    
+             //hide the open accordion items removing the display block style
+             $(menuid+" ul li ul").removeAttr("style");  
+             //Simple hide/show event for the dropdown menus 
+             $(menuid+' li').hover(
+                 function(){
+                 $('ul:first', $(this)).show();
+                 },
+                 function(){
+                 $('ul', $(this)).hide();
+                 }
+              );
+          }             
+     
+        });
       });
     }
-  };
-  	Drupal.behaviors.betterEventSlider = {
-    attach: function(context, settings) {            
-      
-      $(window).load(function(){
-        var liheight = $('ul.event-slider li:first-child').height();
-        $('.bx-wrapper, .bx-wrapper .bx-window').css('height', liheight);
-      });
-      
-      //console.log('woot!');
-      
-      $(window).resize(function(){
-        var liheight = $('ul.event-slider li:first-child').height();
-        $('.bx-wrapper, .bx-wrapper .bx-window').css('height', liheight);
-      });
-      
-      //$('ul.event-slider li').addClass('clearfix');
-    }
-  };
-
-
-	
+  }     	
 })(jQuery);
