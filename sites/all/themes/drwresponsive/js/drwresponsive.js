b0569d860a7ef1c4ed9ce87fed6caadfdaa342f0
Darwin <dbetancourt@drwsoluciones.net>
2013-05-14 17:57:16 -0500

    actualizando archivos del tema drwresponsive

--- sites/all/themes/drwresponsive/js/drwresponsive.js
+++ sites/all/themes/drwresponsive/js/drwresponsive.js
@@ -1,14 +1,28 @@
 (function ($) {
-	Drupal.behaviors.yourfunction = {
+	Drupal.behaviors.menuToggle = {
     attach: function(context) {
       $('body', context).once('accordionmenu', function () {
         $('body').bind('responsivelayout', function (e, d) {        
          
         //Define your drupal menu id (only works with core menu block)
-          var menuid = "#block-system-main-menu";        
+          var menuid = "#block-system-main-menu";
+          // Add Class parent for each li and add icon trough style
+          $(menuid+" li a").each(function() {
+            if ($(this).next().length > 0) {
+                $(this).addClass("parent");
+            };
+          })        
          
         //This condition will act under the 'mobile' size, and will not be executed on ie6 and ie7
           if (d.to == 'mobile' && !$('html').hasClass('ie6') && !$('html').hasClass('ie7')) {       
+
+            // Add menuToggle Link
+            $(menuid).before('<a href="#" class="menu-toggle"><span class="line first-line first"></span><span class="line"></span><span class="line"></span><span class="line last-line last"></span><span class="toggle-help">Menu</span></a>');
+            // Hide main-menu
+            $(menuid).css('display','none');
+            $('.menu-toggle').click(function(){
+                $(menuid).slideToggle();
+            });
            
              //Remove the ´hover´ event in the dropdown menu scrip below              
              $(menuid+' li').unbind();
@@ -28,7 +42,7 @@
           }               
           //this condition will work for all sizes exept mobile, but will act on ie6 and ie7 browsers     
           if (d.to != 'mobile' | $('html').hasClass('ie7') ) {   
-             // $(menuid+" ul.menu li.expanded a").append( $("<span class='list' href='#'>&raquo;</span>") );         
+             //$(menuid+" ul.menu li.expanded a").append( $("<span class='list' href='#'>&raquo;</span>") );             
              //remove the expand elements from the accordion menu             
              $(menuid+" span.over").remove();    
              //hide the open accordion items removing the display block style
